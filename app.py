10fc7628f1367dfb6ab151e9fc06bb19598060ba
raerek <raerek@protonmail.com>
2020-10-19 18:04:03 +0200

    Handling of IMAGE_SRV went in function index(), error handling improved

--- app.py
+++ app.py
@@ -6,32 +6,32 @@ import requests
 IMAGES_FOLDER = os.path.dirname(os.path.realpath(__file__)) + '/images'
 IMAGE_SRV = os.environ.get('IMAGE_SRV', None)
 
-if IMAGE_SRV:
-    ''' If the environment variable IMAGE_SRV exists, try to open it.
-    If no luck exit immediately.
-    '''
-    images_txt_url = 'http://' + IMAGE_SRV + '/images.txt'
-    resp = requests.get(images_txt_url)
-    if resp.status_code == 200:
-        images_in_textfile = [ line for line in resp.text.splitlines()]
-    else:
-        sys.exit('No ' + images_txt_url + ' found.')
-
 app = Bottle()
 
 @app.get('/')
 def index():
     ''' Show the default page.
 
-    Passing image list to the "base.tpl" template, The template creates <img>
-    tags for all filenames in the list.
+    Passing image list and hostname to the "base.tpl" template.
+    The template in retrun displays the hostname in the title,
+    and the images as the content of the page.
+
+    If the environment variable IMAGE_SRV exists, try to open it.
+    If no luck exit immediately.
     '''
+    error_message = None
     if IMAGE_SRV:
-        images = images_in_textfile
+        images_txt_url = 'http://' + IMAGE_SRV + '/images/remote-images.txt'
+        try:
+            resp = requests.get(images_txt_url)
+            images = [ line for line in resp.text.splitlines()]
+        except requests.exceptions.RequestException as e:
+            images = None
+            error_message = 'No ' + images_txt_url + ' found.'
     else:
-        images = os.listdir(IMAGES_FOLDER)
+        images = [ f for f in os.listdir(IMAGES_FOLDER) if os.path.splitext(f)[1].lower() in [".jpg", ".jpeg", ".png"] ]
     hostname = os.uname().nodename # Does not work on Windows.
-    return template('base', images=images, hostname=hostname)
+    return template('base', images=images, hostname=hostname, error_message=error_message)
 
 @app.get('/images/<image>')
 def serve_pictures(image):
@@ -41,9 +41,23 @@ def serve_pictures(image):
     - or proxied from IMAGE_SRV.
     '''
     if IMAGE_SRV:
-        return requests.get('http://' + IMAGE_SRV + '/' + image).content
+        try:
+            # server answered
+            image_url = 'http://' + IMAGE_SRV + '/images/' + image
+            resp = requests.get(image_url)
+            if resp.status_code == 200:
+                return resp.content
+            else:
+                # most likely 404, but we catch all for now
+                raise requests.exceptions.RequestException()
+        except requests.exceptions.RequestException as e:
+            # server did not answer, or bad anser
+            # We send back an image with an error meassage since we are
+            # in the middle of a webpage requesting images.
+            return static_file('proxy_error.png', root=os.path.dirname(os.path.realpath(__file__)))
     else:
+        # sending back local image
         return static_file(image, root=IMAGES_FOLDER)
 
 if __name__ == '__main__':
-    run(app, host = '0.0.0.0', port = 3000, reloader=True, debug=True)
+    run(app, host = '0.0.0.0', port = 3000, reloader=True, debug=False)
